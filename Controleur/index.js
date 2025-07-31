504e50b25e87377299ef70659b178410933174a3
Karate001 <eusty001@gmail.com>
2025-07-31 08:51:13 +0200

    C'est bon dépôt crée

---
+++ Controleur/index.js
@@ -0,0 +1,218 @@
+//Déclaration des variables qui stockeront les produits
+let div_produit= document.getElementById("produits")
+if(div_produit){
+var table_produits_dom= Array.from(div_produit.children)
+
+
+//Déclaration de la page acceuil
+const page_acceuil= document.title;
+if (page_acceuil=="UTOPIA MARKET") {
+
+   /*
+    //Méthode pour supprimer un produit
+    fetch("http://localhost:8080/info_produit/produits/36",{
+          method:"DELETE"  
+        }
+    ).then(res=>{
+        if (res.ok) {
+            console.log("Produit supprimé avec succèqs")
+        }else{console.log("une erreur est survenue lors de la suppression")}
+    }).catch(error=>{console.error(error)} )
+    */
+    
+
+//Méthode qui recupère tous le tableau des produits
+document.addEventListener('DOMContentLoaded',    
+    fetch("http://apiprojete-commerce-env.eba-7hmxxccm.eu-north-1.elasticbeanstalk.com/info_produit/produits")
+    .then(response =>{ 
+            if (!response.ok) {
+               throw new console.error();
+            }
+        return response.json()
+    })
+    .then(produits=>{
+        const table_produits= Object.values(produits)
+        const nbr_prod= table_produits.length
+
+        
+        
+        for (let index = nbr_prod-1; index >= 0; index--) {
+            
+                const prod_aleatoire = Math.floor(Math.random()*(index+1));                          
+                [table_produits[index],table_produits[prod_aleatoire]]=[table_produits[prod_aleatoire],table_produits[index]];
+                    
+            
+        }
+
+        const nbr_prod_final = Math.min(table_produits_dom.length,nbr_prod)
+        for(let i = 0;i<nbr_prod_final; i++){
+            const prod_individuel=table_produits_dom[i] 
+            
+                img_prod= prod_individuel.querySelector("img")               
+                nom_prod= prod_individuel.querySelector("h1") 
+                prix_prod= prod_individuel.querySelector("p");
+
+                
+
+                img_prod.src =table_produits[i].url_image
+                nom_prod.innerText =table_produits[i].nom_produit
+                prix_prod.innerText= "Prix: "+table_produits[i].prix_produit+"$";
+                
+        }
+
+    })
+   
+    );
+
+    table_produits_dom.forEach(produit => {
+        const img_produit=produit.querySelectorAll('img')
+        btn_add_to_cart=produit.querySelectorAll('button')
+        
+        img_produit.forEach(img => {
+            img.addEventListener("click",vers_info_prod)
+        });   
+        
+        btn_add_to_cart.forEach(btn => {
+            btn.addEventListener("click",ajout_prod_panier)
+        });   
+    });
+
+}
+
+}
+ 
+ /*Quand on clique sur un produit page qui contient tous le les info 
+                sur un produit et le bouton commander*/
+function vers_info_prod(event) {
+    
+    prod= event.target.parentElement;
+    img_prod= prod.querySelector("img")
+    nom_prod= prod.querySelector("h1")
+    prix_prod= prod.querySelector("p");
+
+    window.location.href= `page_info_produit.html?nom=${nom_prod.innerText}&prix=${prix_prod.innerText}&url_image=${img_prod.src}`;
+
+}
+
+//Ajout du produit au panier
+let panier=[];
+let compteur_panier=0;
+element_compteur = document.querySelector('.cart-shopping i');
+container_panier= document.querySelector('#container_panier ul');
+
+function ajout_prod_panier(event){
+    event.preventDefault()
+    prod= event.target.parentElement.closest("#prod");
+    
+    img_prod=prod.querySelector('img').src;
+    nom_prod=prod.querySelector('h1').innerText;
+    prix_prod=prod.querySelector('p').innerText;
+    
+    for (let i = 0; i < panier.length; i++) {
+        const element = panier[i];
+        if (img_prod==panier[i].querySelector('img').src) {
+            return;
+        }   
+    }
+    container_panier= document.querySelector('#container_panier ul');
+    prod_li= document.createElement('li');
+    
+    
+    prod_li.innerHTML +=`<img src="${img_prod}" alt="">
+                         <p>${nom_prod}  <br> ${prix_prod}</p>
+                         <div><button style="color:rgb(134, 134, 201);"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
+                            <span>1</span>
+                            <button style="color:rgb(134, 134, 201);"><i class="fa fa-chevron-right" aria-hidden="true"></i></button></div>
+                         <button id="delete_prod_button"><i class="fa fa-trash" aria-hidden="true"></i></button>`;
+        
+    panier.push(prod_li);
+
+    container_panier.appendChild(prod_li);
+    compteur_panier++;
+    element_compteur.setAttribute("data-compteur",compteur_panier);
+    localStorage.setItem("panier", JSON.stringify(panier));
+    localStorage.setItem("compteur_panier",compteur_panier);
+
+    //Gérer la suppression de produit du panier
+    btn_delete_prod= prod_li.querySelector('#delete_prod_button');
+    btn_delete_prod.addEventListener('click',effacer_prod_panier);
+
+     //Le bouton commander
+        creer_btn_commander_consulter();
+
+}
+
+//fonction qui affiche le panier ou le rend invisible
+const icone_panier= document.querySelector('.cart-shopping');
+const element_panier= document.querySelector('#container_panier');
+icone_panier.addEventListener('click',afficher_panier)
+function afficher_panier(){
+    if (element_panier.style.display!="block"&&element_panier.style.visibility!="visible") {
+        element_panier.style.display="block";
+        element_panier.style.visibility="visible";
+        //Le bouton commander
+        creer_btn_commander_consulter();
+    }else{
+        element_panier.style.display="none";
+        element_panier.style.visibility="hidden";
+    }
+}
+
+//Fonction pour suppprimer produit
+function effacer_prod_panier(event) {
+    event.preventDefault;
+    prod_a_supprimer= event.target.closest("li");
+    container_panier.removeChild(prod_a_supprimer);
+    panier.pop(prod_a_supprimer);
+    compteur_panier--;
+    element_compteur.setAttribute("data-compteur",compteur_panier);
+
+     //Le bouton commander
+    creer_btn_commander_consulter();
+}
+
+//Fonction qui gére les boutons commander et consulter nos produits
+function creer_btn_commander_consulter(){
+        if (panier.length===0) {
+            if (!document.getElementById('btn_consulter_prod')) {
+                btn_consulter_prod= document.createElement("button");
+                btn_consulter_prod.setAttribute('id','btn_consulter_prod');
+                btn_consulter_prod.innerHTML=`<a href="#produits">Consultez nos produits</a>`;
+                element_panier.appendChild(btn_consulter_prod);                
+            }            
+            if (document.getElementById('btn_commander')) {
+                element_panier.removeChild(document.getElementById('btn_commander'))
+            }
+        }if (panier.length>0) {
+            if (!document.getElementById('btn_commander')) {
+                btn_commander= document.createElement('button');
+                btn_commander.setAttribute('id','btn_commander');
+                btn_commander.innerText="Commander";
+                element_panier.appendChild(btn_commander);   
+            }
+            if (document.getElementById('btn_consulter_prod')){
+                element_panier.removeChild(document.getElementById('btn_consulter_prod'))
+            }
+        }
+}
+
+
+
+    //Méthode qui ajoute un produit
+/*
+fetch("http://localhost:8080/info_produit/produits", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        nom_produit: "Verre à dégustation (Model:EJ5202)",
+        quantite_produit: "494",
+        prix_produit:"40.00",
+        url_image:"images/images_produits/verre_a_degustation_modele_EJ5201.jpg"
+    })
+    })
+.then(response => response.json())
+.then(data => console.log("Produit ajouté :", data))
+.catch(error => console.error("Erreur :", error))
+*/
---
+++ script.js
@@ -0,0 +1,81 @@
+
+ /*dark_mode_btn */
+    const dark_mode_btn=document.getElementById("dark_mode");
+    dark_mode_btn.addEventListener("click", ()=>{
+        document.body.classList.toggle('dark');
+    });
+    /*menu_btn*/
+    const menu_icon=document.getElementById("menu_icon");
+    const navlinks=document.getElementById("links_menu");
+    menu_icon.addEventListener("click", ()=>{
+        navlinks.classList.toggle('active')
+    })
+    /**compteur experience */
+const counters = document.getElementsByClassName("counter");
+
+Array.from(counters).forEach(counter => {
+    counter.innerText = '+0';
+
+    const update = () => {
+        const target = +counter.getAttribute('data-target');
+        const current = +counter.innerText.replace('+', '');
+        const increment = Math.ceil(target / 300); 
+
+        if (current < target) {
+            const newValue = current + increment;
+            counter.innerText = `+${newValue > target ? target : newValue}`;
+            setTimeout(update, 10); 
+        } else {
+            counter.innerText = `+${target}`; 
+        }
+    };
+
+    update();
+});
+/*compteur promotion*/
+function getNextSunday() {
+  const now = new Date();
+  const day = now.getDay(); // 0 = ce dimanche
+  const diff = (2 - day) % 7;
+
+  const sunday = new Date(now);
+  if (diff === 0 && now.getHours() < 23) {
+    sunday.setDate(sunday.getDate());
+  } else {
+    sunday.setDate(sunday.getDate() + diff);
+  }
+
+  sunday.setHours(23, 59, 59, 999);
+  return sunday.getTime();
+}
+
+const countdownDate = getNextSunday();
+
+const interval = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  if (distance < 0) {
+    clearInterval(interval);
+    document.getElementById("days").innerText = "0";
+    document.getElementById("hours").innerText = "00";
+    document.getElementById("minutes").innerText = "00";
+    document.getElementById("seconde").innerText = "00"; // 
+    return;
+  }
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const secondes = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = String(hours).padStart(2, '0');
+  document.getElementById("minutes").innerText = String(minutes).padStart(2, '0');
+  document.getElementById("seconde").innerText = String(secondes).padStart(2, '0'); // ✅ attention au nom
+}, 1000);
+
+
+
+
+    
